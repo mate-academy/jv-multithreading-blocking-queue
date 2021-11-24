d73198ed0a52b8321fa7e74434011b54f1329535
Roman Legendzevych <green3942@gmail.com>
2021-11-24 22:06:58 +0200

    task complete

--- src/main/java/core/basesyntax/BlockingQueue.java
+++ src/main/java/core/basesyntax/BlockingQueue.java
@@ -12,16 +12,23 @@ public class BlockingQueue<T> {
     }
 
     public synchronized void put(T element) throws InterruptedException {
-        // write your code here
+        while (queue.size() == capacity) {
+            wait();
+        }
+        queue.offer(element);
+        notify();
     }
 
     public synchronized T take() throws InterruptedException {
-        // write your code here
-        return null;
+        while (queue.isEmpty()) {
+            wait();
+        }
+        T tmp = queue.poll();
+        notify();
+        return tmp;
     }
 
     public synchronized boolean isEmpty() {
-        // write your code here
-        return true;
+        return queue.isEmpty();
     }
 }
