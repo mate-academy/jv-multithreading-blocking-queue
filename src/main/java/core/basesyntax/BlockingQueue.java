0d2ab9b841ab1b6c9bb66ab73f17ad96e96e307b
Zakhar Chorny <90692308+zachar-chorny@users.noreply.github.com>
2022-04-13 22:14:09 +0300

    methods of the class BlockingQueue

--- src/main/java/core/basesyntax/BlockingQueue.java
+++ src/main/java/core/basesyntax/BlockingQueue.java
@@ -6,22 +6,31 @@ import java.util.Queue;
 public class BlockingQueue<T> {
     private Queue<T> queue = new LinkedList<>();
     private int capacity;
+    private boolean free = true;
 
     public BlockingQueue(int capacity) {
         this.capacity = capacity;
     }
 
     public synchronized void put(T element) throws InterruptedException {
-        // write your code here
+        while (queue.size() >= capacity) {
+            wait();
+        }
+        queue.add(element);
+        free = false;
+        notify();
     }
 
     public synchronized T take() throws InterruptedException {
-        // write your code here
-        return null;
+        while (free) {
+            wait();
+        }
+        free = false;
+        notify();
+        return queue.poll();
     }
 
     public synchronized boolean isEmpty() {
-        // write your code here
-        return true;
+        return queue.isEmpty();
     }
 }
