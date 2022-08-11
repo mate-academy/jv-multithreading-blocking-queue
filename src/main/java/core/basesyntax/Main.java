326d1fb4f286149ad99696fd69dab810f38f3ecb
gdp <gdp@zigzag.ints.net>
2022-08-11 17:17:46 +0300

    task implemented

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -6,6 +6,7 @@ import core.basesyntax.thread.Producer;
 public class Main {
     public static void main(String[] args) {
         BlockingQueue<Integer> queue = new BlockingQueue<>(10);
+
         Thread producer = new Thread(new Producer(queue));
         Thread consumer = new Thread(new Consumer(queue));
 
