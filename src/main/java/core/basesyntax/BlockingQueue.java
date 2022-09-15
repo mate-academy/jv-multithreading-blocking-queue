d49a08b5c66c9eb279deb38937ed6b9f290a6d0b
Anastasia <anastasian817@gmail.com>
2022-09-15 19:40:04 +0300

    implemented blocking queue

--- src/main/java/core/basesyntax/BlockingQueue.java
+++ src/main/java/core/basesyntax/BlockingQueue.java
@@ -12,16 +12,24 @@ public class BlockingQueue<T> {
     }
 
     public synchronized void put(T element) throws InterruptedException {
-        // write your code here
+        while (capacity < 1) {
+            wait();
+        }
+        queue.add(element);
+        capacity--;
+        notify();
     }
 
     public synchronized T take() throws InterruptedException {
-        // write your code here
-        return null;
+        while (queue.isEmpty()) {
+            wait();
+        }
+        T peek = queue.peek();
+        notify();
+        return peek;
     }
 
     public synchronized boolean isEmpty() {
-        // write your code here
-        return true;
+        return queue.peek() == null;
     }
 }
