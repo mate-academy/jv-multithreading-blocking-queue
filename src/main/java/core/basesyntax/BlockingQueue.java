5d5278ed18b039b710d34ed497cd6cf6c8401329
Aptypio13 <aptypio668@gmail.com>
2022-11-03 08:08:31 +0200

    implement methods

--- src/main/java/core/basesyntax/BlockingQueue.java
+++ src/main/java/core/basesyntax/BlockingQueue.java
@@ -5,23 +5,29 @@ import java.util.Queue;
 
 public class BlockingQueue<T> {
     private Queue<T> queue = new LinkedList<>();
-    private int capacity;
+    private final int capacity;
 
     public BlockingQueue(int capacity) {
         this.capacity = capacity;
     }
 
     public synchronized void put(T element) throws InterruptedException {
-        // write your code here
+        while (queue.size() == capacity) {
+            wait();
+        }
+        notify();
+        queue.add(element);
     }
 
     public synchronized T take() throws InterruptedException {
-        // write your code here
-        return null;
+        while (isEmpty()) {
+            wait();
+        }
+        notify();
+        return queue.poll();
     }
 
     public synchronized boolean isEmpty() {
-        // write your code here
-        return true;
+        return queue.isEmpty();
     }
 }
