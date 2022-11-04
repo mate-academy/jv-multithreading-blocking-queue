0668f5693b14fc4a7c4bfec0eca371d6244c68ac
Yevhen Kupriiuk <kupriuk@gmail.com>
2022-11-04 03:37:31 +0200

    task is completed

--- src/main/java/core/basesyntax/BlockingQueue.java
+++ src/main/java/core/basesyntax/BlockingQueue.java
@@ -1,27 +1,33 @@
 package core.basesyntax;
-
 import java.util.LinkedList;
 import java.util.Queue;
-
 public class BlockingQueue<T> {
     private Queue<T> queue = new LinkedList<>();
     private int capacity;
-
     public BlockingQueue(int capacity) {
         this.capacity = capacity;
     }
 
     public synchronized void put(T element) throws InterruptedException {
-        // write your code here
+        while (queue.size() == capacity) {
+            wait();
+        }
+        System.out.println("Add to queue number " + element);
+        queue.offer(element);
+        notify();
     }
 
     public synchronized T take() throws InterruptedException {
-        // write your code here
-        return null;
+        while (queue.isEmpty()) {
+            wait();
+        }
+        T result = queue.poll();
+        System.out.println("Remove element");
+        notify();
+        return result;
     }
 
     public synchronized boolean isEmpty() {
-        // write your code here
-        return true;
+        return queue.size() == 0;
     }
 }
