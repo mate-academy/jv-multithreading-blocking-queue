df027eabd8622fd4648483d071aecea1059719f5
WallRun <dimonty2003@gmail.com>
2023-06-18 15:58:13 +0300

    Commit 2

--- src/main/java/core/basesyntax/BlockingQueue.java
+++ src/main/java/core/basesyntax/BlockingQueue.java
@@ -6,35 +6,32 @@ import java.util.Queue;
 public class BlockingQueue<T> {
     private final Queue<T> queue = new LinkedList<>();
     private final int capacity;
-    private volatile int size = 0;
 
     public BlockingQueue(int capacity) {
         this.capacity = capacity;
     }
 
     public synchronized void put(T element) throws InterruptedException {
-        while (size >= capacity) {
+        while (queue.size() >= capacity) {
             wait();
         }
 
         notify();
 
         queue.add(element);
-        size++;
     }
 
     public synchronized T take() throws InterruptedException {
-        while (size == 0) {
+        while (queue.size() == 0) {
             wait();
         }
 
         notify();
 
-        size--;
         return queue.remove();
     }
 
     public synchronized boolean isEmpty() {
-       return size == 0;
+       return queue.size() == 0;
     }
 }
