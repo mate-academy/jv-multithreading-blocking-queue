356c3632727d39ecd5a870244cbd9b5810f8ea14
a1111 <okhrymovych11@gmail.com>
2023-08-13 08:08:21 +0300

    Commit Blocking Queue First

--- src/main/java/core/basesyntax/BlockingQueue.java
+++ src/main/java/core/basesyntax/BlockingQueue.java
@@ -4,8 +4,8 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class BlockingQueue<T> {
-    private Queue<T> queue = new LinkedList<>();
-    private int capacity;
+    private final Queue<T> queue = new LinkedList<>();
+    private final int capacity;
 
     public BlockingQueue(int capacity) {
         this.capacity = capacity;
@@ -13,15 +13,26 @@ public class BlockingQueue<T> {
 
     public synchronized void put(T element) throws InterruptedException {
         // write your code here
+        while (queue.size() >= capacity) {
+            wait();
+        }
+        queue.add(element);
+        notify();
     }
 
     public synchronized T take() throws InterruptedException {
         // write your code here
-        return null;
+        while (queue.isEmpty()) {
+            wait();
+        }
+        T type = queue.element();
+        notify();
+        queue.poll();
+        return type;
     }
 
     public synchronized boolean isEmpty() {
         // write your code here
-        return true;
+        return queue.isEmpty();
     }
 }
