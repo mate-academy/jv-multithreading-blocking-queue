0481a2cb921775d0cc770dbc5d23d460bce6025a
Sergey Shestaka <sergey.shestaka@gmail.com>
2024-02-06 15:35:20 +0100

    ssh1 - solution_1

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -4,7 +4,7 @@ import core.basesyntax.thread.Consumer;
 import core.basesyntax.thread.Producer;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         BlockingQueue<Integer> queue = new BlockingQueue<>(10);
         Thread producer = new Thread(new Producer(queue));
         Thread consumer = new Thread(new Consumer(queue));
