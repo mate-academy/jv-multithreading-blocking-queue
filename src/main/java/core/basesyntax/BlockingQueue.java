8e1ba7e7aa21d87d342724eac8ba3280218aa152
MolchanovAlexander <rybkolhoz@gmail.com>
2024-06-13 16:53:14 +0300

    success

--- src/main/java/core/basesyntax/BlockingQueue.java
+++ src/main/java/core/basesyntax/BlockingQueue.java
@@ -1,5 +1,6 @@
 package core.basesyntax;
 
+import java.io.File;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -12,16 +13,27 @@ public class BlockingQueue<T> {
     }
 
     public synchronized void put(T element) throws InterruptedException {
-        // write your code here
+        while (queue.size() == capacity) {
+            this.wait(); // Main thread waits here
+            System.out.println("The put method is waiting");
+        }
+        queue.add(element);
+        this.notifyAll();
     }
 
     public synchronized T take() throws InterruptedException {
-        // write your code here
-        return null;
+       while (isEmpty()) {
+           this.wait(); // Main thread waits here
+           System.out.println("The take method is waiting");
+       }
+       try {
+           return queue.poll();
+       } finally {
+           this.notifyAll();
+       }
     }
 
     public synchronized boolean isEmpty() {
-        // write your code here
-        return true;
+        return queue.size() == 0;
     }
 }
