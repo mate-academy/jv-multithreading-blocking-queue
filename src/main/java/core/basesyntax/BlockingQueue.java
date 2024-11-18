88326340030ae24e461f3e5aa7dbcad01669710d
Vadym <Servicepost.v@gmail.com>
2024-11-18 11:03:42 +0100

    .

--- src/main/java/core/basesyntax/BlockingQueue.java
+++ src/main/java/core/basesyntax/BlockingQueue.java
@@ -12,16 +12,26 @@ public class BlockingQueue<T> {
     }
 
     public synchronized void put(T element) throws InterruptedException {
-        // write your code here
+        while (queue.size() == capacity) {
+            wait();
+        }
+        notifyAll();
+        queue.add(element);
     }
 
     public synchronized T take() throws InterruptedException {
-        // write your code here
-        return null;
+        while (queue.isEmpty()) {
+            wait();
+        }
+        return queue.poll();
     }
 
     public synchronized boolean isEmpty() {
-        // write your code here
-        return true;
+        if (!queue.isEmpty()) {
+            notifyAll();
+            return false;
+        } else {
+            return true;
+        }
     }
 }
