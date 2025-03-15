2d6765c0ce43ab5dcbec713c3dc89866582b4863
Mateusz Seler <mateusz.seler@gmail.com>
2025-03-15 17:48:12 +0100

    implementation

--- src/main/java/core/basesyntax/BlockingQueue.java
+++ src/main/java/core/basesyntax/BlockingQueue.java
@@ -2,26 +2,49 @@ package core.basesyntax;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class BlockingQueue<T> {
     private Queue<T> queue = new LinkedList<>();
+    private Object locker = new Object();
     private int capacity;
+    private AtomicInteger size = new AtomicInteger(0);
 
     public BlockingQueue(int capacity) {
         this.capacity = capacity;
     }
 
-    public synchronized void put(T element) throws InterruptedException {
-        // write your code here
+    public void put(T element) throws InterruptedException {
+        synchronized (locker) {
+            while (isFull()) {
+                locker.wait();
+            }
+
+            queue.add(element);
+            size.incrementAndGet();
+            locker.notifyAll();
+        }
     }
 
-    public synchronized T take() throws InterruptedException {
-        // write your code here
-        return null;
+    public T take() throws InterruptedException {
+        synchronized (locker) {
+            while (isEmpty()) {
+                locker.wait();
+            }
+
+            T peek = queue.peek();
+            size.decrementAndGet();
+            locker.notifyAll();
+
+            return peek;
+        }
+    }
+
+    public synchronized boolean isFull() {
+        return size.get() == capacity;
     }
 
     public synchronized boolean isEmpty() {
-        // write your code here
-        return true;
+        return size.get() == 0;
     }
 }
