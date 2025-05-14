5769c499742a2e7a7c2ea5730e70ef30b3cfe3d8
Katarzyna Ochmanska <kat.ochmanska@gmail.com>
2025-05-14 14:18:38 +0200

    solution

--- src/main/java/core/basesyntax/BlockingQueue.java
+++ src/main/java/core/basesyntax/BlockingQueue.java
@@ -4,24 +4,36 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class BlockingQueue<T> {
-    private Queue<T> queue = new LinkedList<>();
-    private int capacity;
+    private final Queue<T> queue = new LinkedList<>();
+    private final int capacity;
 
     public BlockingQueue(int capacity) {
         this.capacity = capacity;
     }
 
     public synchronized void put(T element) throws InterruptedException {
-        // write your code here
+        try {
+            while (queue.size() == capacity) {
+                wait();
+            }
+            queue.add(element);
+        } finally {
+            notify();
+        }
     }
 
     public synchronized T take() throws InterruptedException {
-        // write your code here
-        return null;
+        try {
+            while (queue.isEmpty()) {
+                wait();
+            }
+            return queue.poll();
+        } finally {
+            notify();
+        }
     }
 
     public synchronized boolean isEmpty() {
-        // write your code here
-        return true;
+        return queue.isEmpty();
     }
 }
