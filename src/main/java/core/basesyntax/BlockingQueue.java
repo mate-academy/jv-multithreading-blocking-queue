fa08c2ea535947071d47aa0bf3ebad405fe2be5d
Dmytro Tkachuk <dmitrotkacuk071@gmail.com>
2025-05-23 21:50:54 +0200

    Completed the task as required.

--- src/main/java/core/basesyntax/BlockingQueue.java
+++ src/main/java/core/basesyntax/BlockingQueue.java
@@ -6,42 +6,30 @@ import java.util.Queue;
 public class BlockingQueue<T> {
     private Queue<T> queue = new LinkedList<>();
     private int capacity;
-    private final Object lok = new Object();
 
     public BlockingQueue(int capacity) {
         this.capacity = capacity;
     }
 
     public synchronized void put(T element) throws InterruptedException {
-        // write your code here
-        synchronized (lok) {
-            while (queue.size() == capacity) {
-                System.out.println("Queue is full, waiting for space");
-                System.out.println("Put method: current capacity: " + queue.size());
-                lok.wait();
-            }
-            lok.notifyAll();
-            queue.add(element);
+        while (queue.size() == capacity) {
+            wait();
         }
+        notifyAll();
+        queue.add(element);
     }
 
     public synchronized T take() throws InterruptedException {
-        // write your code here
-        synchronized (lok) {
-            while (queue.isEmpty()) {
-                System.out.println("Queue is empty, waiting for element");
-                System.out.println("take methode: current capacity: " + queue.size());
-                lok.wait();
-            }
-            lok.notifyAll();
-            return queue.remove();
+
+        while (queue.isEmpty()) {
+            wait();
         }
+        T wer = queue.poll();
+        notifyAll();
+        return wer;
     }
 
     public synchronized boolean isEmpty() {
-        // write your code here
-        System.out.println("Checking is queue empty...");
-        System.out.println("isEmpty method: current capacity: " + queue.size());
         return queue.isEmpty();
     }
 }
